4a432da2d6fd0c54c5aaf4bbc935f8663052bc5f
YOYO3333333 <114952052+YOYO3333333@users.noreply.github.com>
2022-10-18 13:18:01 +0200

    Update itoa.c

--- itoa.c
+++ itoa.c
@@ -1,22 +1,49 @@
 #include <stdio.h>
-void reverse(char *, char *);
+void inverse(char *, char *);
 int len_str(char *);
 char *my_itoa(int value, char *s);
+void decal(char*);
+void decal_min(char* s)
+{
+
+int i=len_str(s);
+while(i)
+{
+    s[i+1]=s[i];
+	i--;
+}
+s[1]=s[0];
+s[0]='-';
+}
 char *my_itoa(int value, char *s)
 {
-    int r, i;
-    r = i = 0;
+    int r, i,min;
+    r = i =min= 0;
+
+    if (!value)
+	{   s[i]=0+'0';
+        s[i+1] = '\0';
+		return s;
+	} 
+	if(min<0)
+	{
+     min++;
+	 value=-value;
+	}
     while (value)
     {
         r = value % 10;
-        s[i] = r;
+        s[i] = r + '0';
         value = value / 10;
         i++;
     }
     s[i] = '\0';
+    inverse(s, s + len_str(s) - 1);
+	if(min)
+     decal_min(s);
     return s;
 }
-void reverse(char *begin, char *end)
+void inverse(char *begin, char *end)
 {
     char temp;
     while (begin < end)
@@ -39,11 +66,9 @@ int len_str(char *s)
 
     return l;
 }
-
 int main(void)
-{
-    char s[] = "";
-    char *temp = s;
-    printf("%s \n", my_itoa(123, s));
+{   char s[]="";
+    printf("%s \n", my_itoa(-123,s));
+
     return 0;
 }
